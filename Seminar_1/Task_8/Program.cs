08cdb6882b92fcfbbd748624aeada7c65f4a739f
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-17 02:20:34 +0300

    Task 8

---
+++ Seminar_1/Task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введённое Вами число: " + N );
+
+int index = 1;
+
+while (index <= N)
+{
+    if (index % 2 == 0) {
+        Console.WriteLine( index );
+    }
+    
+    index += 1;
+}
