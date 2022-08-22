18683d1409f4d08114b170a31e31068ef9f79ad4
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-22 23:12:54 +0300

    Task 10

---
+++ Seminar_2/Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int SecondNumberOf(int number)
+{
+    int result = number % 100 / 10;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+Console.WriteLine("Рандомное число " + number);
+//PartitionOfNumber(number);
+
+Console.WriteLine("Вторая цифра трёхзначного числа: " + SecondNumberOf(number));
