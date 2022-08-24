48f5b3a7566b4457c4a3a0e11c69e193da3c483b
Alona Ratsebarska <aleonorarace@gmail.com>
2022-08-24 16:29:54 +0300

    Task 15

---
+++ Seminar_2/Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+
+Console.WriteLine("Введите, какой сегодня по счёту день недели (число от одного до семи): ");
+int DayOfAWeek = Convert.ToInt32(Console.ReadLine());
+
+if (DayOfAWeek == array[5])
+{
+    Console.WriteLine("Да, поздравляю, сегодня выходной день недели (Суббота).");
+}
+if (DayOfAWeek == array[6])
+{
+    Console.WriteLine("Да, поздравляю, сегодня выходной день недели (Воскресенье).");
+}
+if (DayOfAWeek <= array[4])
+{
+    Console.WriteLine("Нет, сегодня будний день недели. Иди на работу / учёбу :) .");
+}
