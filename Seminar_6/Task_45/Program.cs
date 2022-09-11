c12f0eb8a43dafbff91d96675ce6dd8bc025fbc6
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-11 07:48:33 +0300

    Task 45

---
+++ Seminar_6/Task_45/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 42: Напишите программу,
+которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.Write("Vvedite chislo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string binNumber = Convert.ToString(number,2);
+ 
+Console.WriteLine(binNumber); //выведет - "1100"
