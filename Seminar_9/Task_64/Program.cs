da8bb6dc1c9b4baca52bc55850ad8d3073ac91ec
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-20 12:55:00 +0300

    Task 64

---
+++ Seminar_9/Task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу,
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+Console.WriteLine("Vvvedite pervoje chislo: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvvedite vtoroje chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{PrintNumbers(m, n)}");
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + ", " + PrintNumbers(start + 1, end));
+}
